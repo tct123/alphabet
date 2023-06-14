a8f46983a4baf79d8ee70a0363ef88725681e820
tct123 <42028373+tct123@users.noreply.github.com>
2023-06-14 10:26:52 +0200

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,8 @@
+def rueckwaertz(letters):
+    listerueck=[]
+    letterslength = len(letters)
+    print(letterslength)
+    for letter in letters:
+        pass
+letters = "abcdefghijklmnopqrstuvwxyz"
+rueckwaertz(letters=letters)
