49dac5ad3a3648a057769c93d7a0e7490d4d6793
tct123 <42028373+tct123@users.noreply.github.com>
2023-06-19 11:29:32 +0200

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,14 +1,5 @@
 def rueckwaertz(letters):
-    liste = []
-    lett = len(letters)
-    for letter in letters:
-        liste.append(letter)
-    print(liste)
-    listerueck = []
-    print(lett)
-    for letter in liste:
-    	listerueck.append(liste[lett])
-    	lett = lett-1
-    print(listerueck)
-letters = "abcdefghijklmnopqrstuvwxyz"
+    letters = letters [::-1]
+    print(letters)
+letters = input()
 rueckwaertz(letters=letters)
